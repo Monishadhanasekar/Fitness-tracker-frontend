a9f245c4e5e0e8e329bd1cc09caa29c690c9a899
Monishadhanasekar <97545391+Monishadhanasekar@users.noreply.github.com>
2022-06-16 18:47:39 +0530

    frontend files added

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route} from "react-router-dom";
+
+import Navbar from "./components/navbar.component"
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
+import { ForkRight, ForkRightTwoTone, Gradient } from '@mui/icons-material';
+
+function App() {
+  return (
+    <Router>
+     
+      <div style={{backgroundColor:"lightpink",height:"700px"}} className="container">
+      <Navbar />
+      <br/>
+      <Route path="/" exact component={ExercisesList} />
+      <Route path="/edit/:id" component={EditExercise} />
+      <Route path="/create" component={CreateExercise} />
+      <Route path="/user" component={CreateUser} />
+      </div>
+    
+    </Router>
+  );
+}
+
+export default App;
